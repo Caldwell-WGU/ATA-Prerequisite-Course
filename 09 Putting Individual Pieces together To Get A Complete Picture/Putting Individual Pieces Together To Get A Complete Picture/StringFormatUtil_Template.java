58a41af2966642b3342c6638a2bdaa82ba0251ed
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 18:31:35 -0500

    Create StringFormatUtil_Template.java

---
+++ 09 Putting Individual Pieces together To Get A Complete Picture/Putting Individual Pieces Together To Get A Complete Picture/StringFormatUtil_Template.java
@@ -0,0 +1,29 @@
+package com.ata;
+
+public class StringFormatUtil {
+
+    /**
+     * Formats a price so that different size values start with a dollar sign and
+     * are right aligned.
+     * 
+     * @param price a value to be formatted
+     * @return formatted string
+     */
+    public static String formatPriceLine(double price) {
+        return null;
+    }
+
+    /**
+     * Formats input so that a label is shown above a value and its respective
+     * formatted form.
+     * 
+     * @param label a heading for the value to be formatted
+     * @param value a number to be formatted
+     * @return formatted string
+     */
+    public static String formatLabeledValue(String label, int value) {
+        return null;
+
+    }
+
+}
