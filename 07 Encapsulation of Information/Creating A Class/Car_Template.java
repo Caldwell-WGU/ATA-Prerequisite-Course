721e20ad76d1874c5e1fc10c70c9c566cd12a5ed
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 17:54:25 -0500

    Create Car_Template.java

---
+++ 07 Encapsulation of Information/Creating A Class/Car_Template.java
@@ -0,0 +1,20 @@
+package com.ata;
+
+public class Car {
+    // TODO: Add attributes from UML diagram below
+
+
+
+
+    // TODO: Add constructor from UML diagram below
+
+
+    // TODO: Add getter methods from UML diagram below
+
+
+    // TODO: Add the start method from UML below
+
+
+    // TODO: Add the stop method from UML below
+
+}
