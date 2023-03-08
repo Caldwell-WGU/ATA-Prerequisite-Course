cc820916238b7360543cac5b7a8b433d4d599db6
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 18:48:34 -0500

    Create URLParser_Template.java

---
+++ 09 Putting Individual Pieces together To Get A Complete Picture/Working With StringBuilder and Substrings/URLParser_Template.java
@@ -0,0 +1,38 @@
+package com.ata;
+
+public class URLParser {
+
+    /*
+    * Gets the protocol of an url
+    *
+    * @param url the url as a String
+    * @return the protocol part of the url
+    * */
+    public String getProtocol(String url) {
+        // TODO: Step 1 - Replace return null to return the protocol of url
+        return null;
+    }
+
+    /*
+     * Gets the hostname of an url
+     *
+     * @param url the url as a String
+     * @return the hostname part of the url
+     * */
+    public String getHostname(String url) {
+        // TODO: Step 2 - Replace return null to return the hostname of url
+        return null;
+    }
+
+    /*
+     * Gets the path of an url
+     *
+     * @param url the url as a String
+     * @return the path part of the url
+     * */
+    public String getPath(String url) {
+        // TODO: Step 3 - Replace return null to return the path of url
+        return null;
+    }
+
+}
