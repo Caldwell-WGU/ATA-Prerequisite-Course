048f492bd4a555ace0900f50fe72bddcaf36e5f7
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 18:15:42 -0500

    Create TodoList_Template.java

---
+++ 08 Capturing Information While Running Code/ArrayLists and Arrays/TodoList_Template.java
@@ -0,0 +1,26 @@
+package com.ata;
+
+public class TodoList {
+
+    // TODO: Declare attributes below
+
+
+    // TODO: Create constructor below
+
+
+    // TODO: Create addItem method below
+
+
+    // TODO: create getNumberOfItems below
+
+
+    // TODO: create printTodo below
+
+
+    // TODO: create removeItem below
+
+
+    // TODO: Create changeItem below
+
+
+}
