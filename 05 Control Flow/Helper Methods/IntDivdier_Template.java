6b0c32054b5caa4ddb1a884b12558039a1ca0878
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 16:40:11 -0500

    Create IntDivdier_Template.java

---
+++ 05 Control Flow/Helper Methods/IntDivdier_Template.java
@@ -0,0 +1,31 @@
+package com.ata;
+
+public class IntDivider {
+    // TODO: Step 3.2 Add instance variable
+
+    /**
+     * Step 1: This method divides an integer value by another integer value.
+     * 
+     * @param integer1 number to be divided
+     * @param integer2  number to divide the dividend by
+     * @return quotient or result of the division
+     */
+
+
+    /**
+     * Step 2: This method displays the calculation result.
+     */
+
+
+    /**
+     * Step 3: This method calls other methods to do a calculation then display the
+     * result.
+     * 
+     * @param number1 first integer in calculation
+     * @param number2 second integer in calculation
+     */
+    void calculate(int number1, int number2) {
+
+    }
+
+}
