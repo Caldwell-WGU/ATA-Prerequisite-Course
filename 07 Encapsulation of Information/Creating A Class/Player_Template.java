6f7f3ff5e8f8eee3da7186401308e10607549b0c
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 17:55:03 -0500

    Create Player_Template.java

---
+++ 07 Encapsulation of Information/Creating A Class/Player_Template.java
@@ -0,0 +1,14 @@
+package com.ata;
+
+public class Player {
+    // TODO: Add attributes from UML diagram below
+
+
+    // TODO: Add constructor from UML diagram below
+
+
+    // TODO: Add method to call the Car class start method.
+
+
+    // TODO: Add method to call the Car class stop method.
+}
