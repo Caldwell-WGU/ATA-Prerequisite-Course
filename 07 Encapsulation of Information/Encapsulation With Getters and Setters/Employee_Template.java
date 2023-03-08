36506627bad8424e1bfe7a81790960921c0ce8db
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 17:23:03 -0500

    Create Employee_Template.java

---
+++ 07 Encapsulation of Information/Encapsulation With Getters and Setters/Employee_Template.java
@@ -0,0 +1,21 @@
+package com.ata;
+
+public class Employee {
+
+    // TODO: Step 1 make these attributes private.
+    String name;
+    int numberOfCompanyShares;
+
+    public Employee(String name, int numberOfCompanyShares) {
+        // TODO: Step 2 set the attributes to the values of the arguments using "this".
+    }
+
+    // TODO: Step 3 add setter methods for the attributes.
+
+
+    // TODO: Step 4 add getter methods for the attributes.
+
+
+    // TODO: TODO: Step 5 add and implement the addShares method.
+
+}
