827c114c39f539efeb663cf1ff174c96d84beaca
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 16:48:22 -0500

    Create QualityChecker_Template.java

---
+++ 05 Control Flow/Stop Using Your Resources Once You Have Your Answer/QualityChecker_Template.java
@@ -0,0 +1,13 @@
+package com.ata;
+
+public class QualityChecker {
+
+    /*
+     * Calculates the average sugar content of a batch of fruit punch before a certain threshold
+     * @param sugarContents a double array containing the sugar content of a batch of fruit punch
+     * @param threshold     the threshold that the sugar content needs to be below
+     * @return              the average sugar content before a threshold
+     * */
+    // TODO: Create qcMachine method below this.
+
+}
