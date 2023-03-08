6231d27b0825d9ed7f04f7db1c7a826ca99d6073
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 17:13:41 -0500

    Create ObjectInstantiation_Template.java

---
+++ 06 Simplifying and Consolidating Your Code/Deciding Between Public and Private Modifiers/ObjectInstantiation_Template.java
@@ -0,0 +1,20 @@
+package com.ata;
+
+public class ObjectInstantiation {
+
+    // TODO: Step 2.1: Declare a private Form object below called myForm.
+
+
+    // TODO: Step 1: Create method createEmptyForm below
+
+
+    // TODO: Step 2.2: Create method instantiateMyForm below
+
+
+    // TODO: Step 3.1: Create method createTempObj below
+
+
+    // TODO: Step 3.2: Create method createTempObj2 below
+
+    
+}
