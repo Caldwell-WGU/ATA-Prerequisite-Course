0378b94b849d4bd47072601e3dc23a35733f26ca
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 15:57:53 -0500

    Create Recipe_Template.java

---
+++ 04 Getting Started With Java/Introduction to Variables/Recipe_Template.java
@@ -0,0 +1,41 @@
+
+package com.ata;
+
+public class Recipe {
+    // TODO: Add Step 1 and Step 2 variables below
+
+
+
+
+
+
+
+    // End of variables
+
+    /**
+     * This method should initialize the numberOfIngredients variable. It should use
+     * the length of the ingredients array.
+     */
+    public void initializeVariable() {
+        // TODO: Step 3.1 on the line below
+
+    }
+
+    /**
+     * This method should print the title variable. It should use the System println
+     * method provided.
+     */
+    public void printVariable() {
+        // TODO: Step 3.2 on the line below
+        System.out.println();
+    }
+
+    /**
+     * This method should update an element value in the ingredients array variable.
+     */
+    public void updateVariable() {
+        // TODO: Step 3.3 on the line below
+
+    }
+    
+}
