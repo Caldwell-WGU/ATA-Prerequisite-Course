8a9de6092922b8c220c8806ebb8b1e64b1fa5149
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 17:58:57 -0500

    Create PollyBank_Submission.java

---
+++ 08 Capturing Information While Running Code/User Input and While Loops/PollyBank_Submission.java
@@ -0,0 +1,51 @@
+package com.ata;
+import java.util.Scanner;
+
+public class PollyBank {
+    
+    double balance = 0.0;
+    String phrase = "";
+    int repeatNumber = 0;
+    // TODO: Step 1 - add Scanner type variable
+    Scanner keyboard = new Scanner(System.in);
+    /**
+     * Modifies the text that is mimicked by the bank.
+     */
+    public void updatePhrase() {
+
+        System.out.println("What is the phrase to mimic?");
+        // TODO: Step 2.1 - capture a line of text to be repeated
+        phrase = keyboard.nextLine();
+
+        System.out.println("How many times should it be repeated?");
+        // TODO: Step 2.2 - capture the number of times for the text to be repeated
+        repeatNumber = keyboard.nextInt();
+        
+        // TODO: Step 2.3 - move the Scanner object to the next line
+        keyboard.nextLine();
+    }
+    
+    /**
+     * Adds various amounts to the bank balance.
+     */
+    public void addMoney() {
+        System.out.println("How much money is being added to Polly?");
+
+        // TODO: Step 3 - add a while loop for adding money
+        while (keyboard.hasNextDouble()){
+            balance += keyboard.nextDouble();
+            keyboard.nextLine();
+            int loopNumber = repeatNumber;
+            while(loopNumber > 0){
+                System.out.println(phrase);
+                loopNumber--;
+            }
+            System.out.println("Enter another amount to add or any other key to stop");
+        }
+        
+        System.out.print("Polly Bank Balance is $");
+        System.out.println(balance);
+        System.out.println("Goodbye");
+    }
+
+}
