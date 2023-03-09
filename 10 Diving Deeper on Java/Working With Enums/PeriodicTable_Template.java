3c7e525926793de2c30f2b61d39fbfd3ef652524
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 19:22:43 -0500

    Create PeriodicTable_Template.java

---
+++ 10 Diving Deeper on Java/Working With Enums/PeriodicTable_Template.java
@@ -0,0 +1,39 @@
+package com.ata;
+
+import java.util.ArrayList;
+
+public class PeriodicTable {
+
+    /**
+     * Translate an Element enum periodic symbol to the corresponding element string.
+     *
+     * @param element the Element enum, a single character symbol
+     * @return the corresponding element string
+     * */
+    public String translateSymbol(Element element) {
+        // TODO: Step 2: Add logic to return element string before return null;
+        return null;
+    }
+
+    /**
+     * Translate an Element enum periodic symbol to the corresponding element string.
+     *
+     * @param elementStr a single character string representing the symbol
+     * @return the corresponding element string
+     * */
+    public String translateSymbol(String elementStr) {
+        // TODO: Step 3: Replace return null with logic for overloaded translateSymbol.
+        return null;
+    }
+
+    /**
+     * Gets the element string for all the values in Element.
+     *
+     * @return An ArrayList of Strings
+     * */
+    public ArrayList<String> getElementsOfLife() {
+        // TODO: Step 4: Replace return null with logic for getElementsOfLife
+        return null;
+    }
+
+}
