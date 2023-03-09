a0e7cc649be36fcdefbf3a4b71dff2a70cf39758
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 19:51:21 -0500

    Create Menu.java

---
+++ Project: T-Shirt Store/Template/Menu.java
@@ -0,0 +1,88 @@
+package com.ata;
+
+import java.util.Scanner;
+
+/**
+ * Menu class is responsible for greeting the user, showing a menu
+ * to the user for interaction with the application.
+ * The class maintains a String[] array of menu options, which it
+ * uses for printing the menu to standard out.
+ * The class is also responsible for responding to user interaction
+ * with respect to its menu.
+ */
+public class Menu {
+    private String[] menuOptions = {};
+    private Scanner scanner;
+
+    /**
+     * Class constructor, which expects a Scanner object
+     * It allows for the user to specify how a Scanner is to
+     * be configured outside this class and for basic inversion of control.
+     * @param scanner takes in a Scanner object to initialize
+     */
+    public Menu(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+
+    /**
+     * Prints the menu to standard output, using the class's menuOptions array.
+     * It accepts user input using the encapsulated Scanner instance.
+     * All output is printed to standard out.
+     * All input is collected from standard in.
+     */
+    public void executeMenu() {
+        printMenu();
+        exit();
+    }
+
+    /**
+     * Asks the user to enter their name.
+     * After the user provides their name, it outputs a greeting to the user to standard out.
+     */
+    public void greet() {
+        String name = scanner.nextLine();
+
+        System.out.println("Welcome " + name + " to " + "DEFAULT SHOP NAME");
+    }
+
+    /**
+     * Prints the menu header and menu options.
+     */
+    private void printMenu() {
+        System.out.println();
+        System.out.println("--Main Menu--");
+        System.out.println("Select an option using one of the numbers shown");
+        System.out.println();
+
+        for (int i = 0; i < menuOptions.length; i++) {
+            System.out.print(i + ": ");
+            System.out.println(menuOptions[i]);
+        }
+    }
+
+    /**
+     * Prints an exit statement and closes the scanner object.
+     */
+    private void exit() {
+        scanner.close();
+    }
+
+    /**
+     * Collects next user-entered int.
+     * @return integer value denoting the user selection
+     */
+    private int getNextIntFromUser() {
+        return scanner.nextInt();
+    }
+
+    /**
+     * Skips a line of empty input from the scanner's input stream
+     * and then returns the next available line.
+     * @return string representing the line of input typed by the user
+     */
+    private String getNextStringLineFromUser() {
+        scanner.nextLine();
+        return scanner.nextLine();
+    }
+}
