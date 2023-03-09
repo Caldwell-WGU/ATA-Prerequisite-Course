e8965debaf81663caef55f1046fe0a99324a3df0
Caldwell-WGU <127269532+Caldwell-WGU@users.noreply.github.com>
2023-03-08 19:16:49 -0500

    Create Person_Template.java

---
+++ 10 Diving Deeper on Java/Reusing Code/Person_Template.java
@@ -0,0 +1,28 @@
+package com.ata;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    /**
+     * Initializes a Person object.
+     *
+     * @param firstName the first name of the person
+     * @param lastName the last name of the person
+     * @param age the age of the person
+     * */
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    // TODO: Step 1: Create the equals method
+
+
+    // TODO: Step 2: Create the hashCode method
+
+
+}
